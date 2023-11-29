a3d5e7941b506013c5121a4afa37cf91b34449b4
TheReal19 <133283840+TheReal19@users.noreply.github.com>
2023-11-29 19:58:09 +0000

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -8,12 +8,12 @@ fig = go.Figure(data=go.Choropleth(
     locations=df['code'], # Spatial coordinates
     z = df['number students'].astype(float), # Data to be color-coded
     locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Hot',
-    colorbar_title = "Visited by APCSP students",
+    colorscale = 'blues',
+    colorbar_title = "APCSP Students Locations Visited",
 ))
 
 fig.update_layout(
-    title_text = 'Visited States US Choropleth Map',
+    title_text = 'Places APCSP students have visited',
     geo_scope='usa', # limite map scope to USA
 )
 
